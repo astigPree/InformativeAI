ac373ae6468c1ab96010dfb559fbcd4c055580be
astigPree <121760681+astigPree@users.noreply.github.com>
2023-03-24 16:05:09 +0800

    update datas

--- Datas/script.py
+++ Datas/script.py
@@ -6,6 +6,7 @@
 
 import os
 import json
+import random
 
 datas = {"intents" :[]}
 
@@ -14,20 +15,28 @@ not_valid = [ "script.py" , "over all data.json" , "answer.txt"]
 
 def get_all_text(file : str) -> list :
 	texts = []
-	invalid = ["\n" , "" , "\t"]
+	invalid = ["\n" , "" , "\t" ]
+	invalid_letter = ( "," ,"?")
 	with open(file , 'r' ) as f :
 		for text in f.read().split("\n") :
 			if text in invalid :
 				continue
-			texts.append(text.lower())
+				
+			cleaned = ""
+			for letter in text :
+				if letter in invalid_letter :
+					continue
+				cleaned += letter
+				
+			texts.append(cleaned.lower())
+			
 	return texts
 
 
 for file in os.listdir(os.getcwd()):
 	data = {"tag": "",
 	    "patterns": [],
-	    "responses": [],
-	    "context_set": ""
+	    "responses": []
 	  }
 	if file not in not_valid :
 		data["tag"] = os.path.splitext(file)[0]
@@ -35,8 +44,35 @@ for file in os.listdir(os.getcwd()):
 		data["responses"] = [os.path.splitext(file)[0]]
 		
 		datas["intents"].append(data)
-		print(file)
+
+
+highest = []
+for key , value in datas.items():
+	for v in value :
+		highest.append( len(v["patterns"]) )
+
+highest = max(highest)
+for key , value in datas.items():
+	for v in value :
+		if len(v["patterns"]) < highest :
+			count = highest - len(v["patterns"]) 
+			for _ in range(count) :
+				v["patterns"].append(random.choice(v["patterns"]))
 	
+
+for key , value in datas.items():
+	for v in value :
+		if len(v["patterns"]) != highest :
+			print(v["tags"])
+
+
+for key , value in datas.items():
+	for v in value :
+		if len(v["patterns"]) == highest :
+			print(f"{v['tag']} : {len(v['patterns'])}  : {len(v['responses'])} ")
+			
+
+
 
 with open(not_valid[1] , "w") as jf:
 	json.dump(datas , jf , indent=4)
